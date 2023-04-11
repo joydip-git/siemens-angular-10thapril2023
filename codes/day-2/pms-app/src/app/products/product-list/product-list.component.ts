49b603f87c641c60e790f78387bff86695844dda
Joydip <joydip.mondal@gmail.com>
2023-04-11 14:54:31 +0530

    product-list component created

---
+++ codes/day-2/pms-app/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { products } from "src/data/products";
+import { Product } from "src/models/product";
+
+@Component({
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+    productRecords: Product[] = products
+}
