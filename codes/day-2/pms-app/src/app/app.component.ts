c285694547db01d29ca67f893b4e12a45064aaae
Joydip <joydip.mondal@gmail.com>
2023-04-11 14:03:06 +0530

    pms-app products array and model created

--- codes/day-2/pms-app/src/app/app.component.ts
+++ codes/day-2/pms-app/src/app/app.component.ts
@@ -7,26 +7,4 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
     title = "Product Management"
-    numbers = [1, 2, 3]
-    person = {
-        name: 'anil',
-        salary: 1000
-    }
-    people = [
-        {
-            name: 'anil',
-            salary: 1000,
-            friends: ['sunil', 'mahesh']
-        },
-        {
-            name: 'sunil',
-            salary: 2000,
-            friends: []
-        }
-    ]
-    // constructor() {
-    //     for (let num of this.numbers) {
-    //         console.log(num)
-    //     }
-    // }
 }
