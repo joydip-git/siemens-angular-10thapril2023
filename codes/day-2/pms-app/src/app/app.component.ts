98ea14b4e17456f773b58ed6cebaced9abbfd122
Joydip <joydip.mondal@gmail.com>
2023-04-11 12:31:53 +0530

    component data binding and directives discussed

--- codes/day-2/pms-app/src/app/app.component.ts
+++ codes/day-2/pms-app/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from "@angular/core";
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+    title = "Product Management"
+    numbers = [1, 2, 3]
+    person = {
+        name: 'anil',
+        salary: 1000
+    }
+    people = [
+        {
+            name: 'anil',
+            salary: 1000,
+            friends: ['sunil', 'mahesh']
+        },
+        {
+            name: 'sunil',
+            salary: 2000,
+            friends: []
+        }
+    ]
+    // constructor() {
+    //     for (let num of this.numbers) {
+    //         console.log(num)
+    //     }
+    // }
 }
